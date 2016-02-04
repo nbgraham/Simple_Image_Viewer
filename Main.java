d0c55a3238892c958ca07d5bccc4d8b732adaa55
nbgraham <nickbgraham83@gmail.com>
2016-02-03 19:25:56 -0600

    Working 5 ways

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
@@ -10,7 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import javax.swing.Action;
+import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -20,7 +22,10 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerListModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * It's bad form to create an entire program in a single class
@@ -31,11 +36,33 @@ import javax.swing.KeyStroke;
  * @author Nicholas Graham 113038384
  */
 public class Main {
-	//Create a new JFrame (i.e. an application window
+	
+	static class NextPictureAction extends AbstractAction {
+	    public NextPictureAction() {
+	        super("Next->");
+	        putValue(SHORT_DESCRIPTION, "Move to the next picture");
+	        putValue(MNEMONIC_KEY, new Integer(KeyEvent.VK_RIGHT));
+	    }
+	    public void actionPerformed(ActionEvent e) {
+	        nextImage();
+	    }
+	}
+	
+	static class PrevPictureAction extends AbstractAction {
+	    public PrevPictureAction() {
+	        super("<-Previous");
+	        putValue(SHORT_DESCRIPTION, "Move to the previous picture");
+	        putValue(MNEMONIC_KEY, new Integer(KeyEvent.VK_LEFT));
+	    }
+	    public void actionPerformed(ActionEvent e) {
+	        prevImage();
+	    }
+	}
+	
 	final static JFrame frame = new JFrame();
 	final static JLabel pictureLabel = new JLabel();
 	final static List<File> pictureFiles = new ArrayList<File>();
-	final static List<Integer> currentFileIndex = new ArrayList<Integer>();
+	final static SpinnerListModel picturesList = new SpinnerListModel();
 
 	public static void main( final String[] args ) {
 
@@ -45,47 +72,72 @@ public class Main {
 		frame.setSize( 600, 500 );
 		//give the window a title
 		frame.setTitle( "Image Viewer" );
-		
+	
 		/*
 		final ImageIcon icon = new ImageIcon( "/Users/Nicholas/Pictures/Picture1.png" );
 		icon.setImage( icon.getImage().getScaledInstance(
 				30, 30, Image.SCALE_FAST ) );
 		pictureLabel.setIcon( icon );
 		*/
-		
-		currentFileIndex.add(new Integer(0));
-		
-		final JButton nextButton = new JButton("Next->");
-		final JButton prevButton = new JButton("<-Previous");
-		
-		nextButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				nextImage();
-			}
-			
-		});
-		
-		prevButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				prevImage();
-			}
-			
-		});
+				
+		final JButton nextButton = new JButton(new NextPictureAction());
+		final JButton prevButton = new JButton(new PrevPictureAction());
 		
 		final JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout());
 		buttonPanel.add(prevButton);
 		buttonPanel.add(nextButton);
 		
-		frame.getContentPane().setLayout( new BorderLayout());
+		final JSpinner pictureSpinner = new JSpinner(picturesList);
+		pictureSpinner.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent arg0) {
+				updateImage();
+			}
+			
+		});
 		
+		pictureLabel.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				nextImage();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		frame.getContentPane().setLayout( new BorderLayout());
 		//add the various components to the window's contents;
-		frame.getContentPane().add( pictureLabel, BorderLayout.CENTER );
+		frame.getContentPane().add(pictureLabel, BorderLayout.CENTER );
 		frame.getContentPane().add(buttonPanel, BorderLayout.PAGE_END);
+		frame.getContentPane().add(pictureSpinner, BorderLayout.PAGE_START);
+
 		
 		//create a menu bar, menu, and two menu items:
 		//one which opens an "Open Directory" dialog, and one which
@@ -102,6 +154,10 @@ public class Main {
 			}
 		});
 		
+		final JMenu pictureMenu = new JMenu("Picture");
+		final JMenuItem nextPicture = new JMenuItem(new NextPictureAction());
+		final JMenuItem prevPicture = new JMenuItem(new PrevPictureAction());
+		
 		JMenuItem open = new JMenuItem();
 		open.setText( "Open" );
 		open.addActionListener( new ActionListener() {
@@ -115,11 +171,8 @@ public class Main {
 				if( jfc.showOpenDialog( frame ) == JFileChooser.APPROVE_OPTION ) {
 					pictureFiles.clear();
 					pictureFiles.addAll(getPicturesInDirectory(jfc.getSelectedFile().getAbsolutePath()));
-					if (pictureFiles.size() > 0)
-					{
-						currentFileIndex.set(0, new Integer(0));
-						updateImage();
-					}
+					picturesList.setList(pictureFiles);
+					updateImage();
 				}
 			}
 		});
@@ -127,6 +180,10 @@ public class Main {
 		fileMenu.add( open );
 		fileMenu.add( close );
 		menuBar.add( fileMenu );
+		
+		pictureMenu.add(nextPicture);
+		pictureMenu.add(prevPicture);
+		menuBar.add(pictureMenu);
 		
 		frame.setJMenuBar( menuBar );
 		
@@ -152,12 +209,7 @@ public class Main {
 	{
 		if (pictureFiles.size() > 0)
 		{
-			Integer nextValue = currentFileIndex.get(0) + 1;
-			if (nextValue.compareTo(new Integer(pictureFiles.size())) >= 0)
-			{
-				nextValue = new Integer(0);
-			}
-			currentFileIndex.set(0, nextValue);
+			picturesList.setValue(picturesList.getNextValue());
 			updateImage();
 		}
 	}
@@ -166,19 +218,17 @@ public class Main {
 	{
 		if (pictureFiles.size() > 0)
 		{
-			Integer prevValue = currentFileIndex.get(0) - 1;
-			if (prevValue.compareTo(new Integer(0)) < 0)
-			{
-				prevValue = new Integer(pictureFiles.size()-1);
-			}
-			currentFileIndex.set(0, prevValue);
+			picturesList.setValue(picturesList.getPreviousValue());
 			updateImage();
 		}
 	}
 	
 	public static void updateImage()
 	{
-		pictureLabel.setIcon(new ImageIcon(pictureFiles.get(currentFileIndex.get(0)).getAbsolutePath()));
+		if (pictureFiles.size() > 0)
+		{
+			pictureLabel.setIcon(new ImageIcon(((File)picturesList.getValue()).getAbsolutePath()));
+		}
 	}
 }
 
